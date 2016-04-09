3ca7625de187f59ff12ee9d8119054ba5a57a028
Flavio Cassini <fbcassini@fullsail.edu>
2016-04-09 12:22:56 -0400

    add second component
    added second component

--- WSP_Cassini_Flavio_Wk1/js/main.js
+++ WSP_Cassini_Flavio_Wk1/js/main.js
@@ -3,3 +3,8 @@
     $(function(){
       $("#includedContent").load("components/1/component.html"); 
     });
+
+
+ $(function(){
+      $("#includedContent2").load("components/2/component2.html"); 
+    });
