2c7eb6681607d266729ff4a50f8d07f0c6a99846
Flavio Cassini <fbcassini@fullsail.edu>
2016-04-10 08:00:19 -0400

    Finished
    Finished

--- WSP_Cassini_Flavio_Wk1/js/main.js
+++ WSP_Cassini_Flavio_Wk1/js/main.js
@@ -8,3 +8,15 @@
  $(function(){
       $("#includedContent2").load("components/2/component2.html"); 
     });
+	
+	 $(function(){
+      $("#includedContent3").load("components/3/component3.html"); 
+    });
+	
+	$(function(){
+      $("#includedContent4").load("components/4/component4.html"); 
+    });
+	
+	$(function(){
+      $("#includedContent5").load("components/5/component5.html"); 
+    });
